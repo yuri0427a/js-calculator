4ff01157a8f9e1129e7777df5adc3e685baa3687
yuri0427a <yuri0427ita@gmail.com>
2021-09-02 00:54:46 +0900

    [Add]電卓機能　途中

--- index.js
+++ index.js
@@ -2,3 +2,33 @@ myTotal = 0;
 myInput = "";
 myCalc = "+";
 myFlg = 1;
+
+function myValue(myData) {
+    myFlg = 0;
+    myInput += myData;
+    document.myForm.myLine.value = myInput;
+}
+
+function myCalculate(myData) {
+    if (myFlg == 0) {
+        myFlg = 1;
+        myWork = myTotal + myCalc + myInput;
+        myTotal = Function(myWork)();
+        myInput = "";
+        document.myForm.myLine.value = myTotal;
+    }
+    if (myDate == "=") {
+        myTotal = 0;
+        myCalc = "+";
+    } else {
+        myCalc = myData;
+    }
+}
+
+
+function myC() {
+    myTotal = 0;
+    myCalc = "+";
+    myInput = "";
+    document.myForm.myLine.value = myTotal;
+}
